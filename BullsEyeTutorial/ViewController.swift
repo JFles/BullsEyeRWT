9668cc21e960583d72ef76f19c1ff04565f5ded7
Jeremy Fleshman <jeremyfleshman@jeremys-macbook-pro.local>
2018-03-13 20:58:32 -0400

    built out unstyled UI, added slider functionality. Code accurate to ch3

--- BullsEyeTutorial/ViewController.swift
+++ BullsEyeTutorial/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 50 // magic number for init slider val -- is there a way to init w/ slider val instead?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func showAlert(){
+        let message = "The value of the slider is: \(currentValue)"
+        
+        
+        let alert = UIAlertController(title: "Hello, World!",
+                                    message: message,           //changed
+                             preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK",                 //changed
+                                   style: .default,
+                                 handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
+    @IBAction func sliderMoved(_ slider: UISlider){
+//        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
 }
 
