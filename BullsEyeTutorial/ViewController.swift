c5cbc0d72eb4d0d756a7cd7b739fcc227eadb993
Jeremy Fleshman <jfleshman@outsitenetworks.com>
2018-05-05 09:55:33 -0400

    code accurate through ch 5. Game's general function is complete. Needs polish and some additional behaviors

--- BullsEyeTutorial/ViewController.swift
+++ BullsEyeTutorial/ViewController.swift
@@ -11,10 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     var currentValue: Int = 0 // have to initialize var to some value
     var targetValue: Int = 0
+    var score: Int = 0
+    var round: Int = 0
     
     //instance variable exposed to IB to connect to a UISlider obj
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +31,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert(){
-        let message = "The value of the slider is: \(currentValue)" +
-                      "\nThe target value is: \(targetValue)"
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        let message = "You scored \(points) points!" +
+                      "\nScore = \(score)"
+        
+//        let message = "The value of the slider is: \(currentValue)" +
+//                      "\nThe target value is: \(targetValue)" +
+//                      "\nThe difference between the values is: \(difference)"
         
         
         let alert = UIAlertController(title: "Hello, World!",
@@ -54,12 +67,15 @@ class ViewController: UIViewController {
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
+        round += 1
         updateLabels()
     }
 
     func updateLabels(){
 //        targetLabel.text = String(targetValue) // same as
         targetLabel.text = "\(targetValue)"
+        scoreLabel.text = "\(score)"
+        roundLabel.text = "\(round)"
     }
 }
 
